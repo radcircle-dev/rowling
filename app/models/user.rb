82ba341d09dc92495cbacf6f5ec1739f57b5c6da
Paul Millar <paul@digitaldawn.com.au>
2014-09-25 15:00:45 +1000

    Added basic roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,22 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  ROLES = %w(admin editor author intern)
+
+  def admin?
+    role == 'admin'
+  end
+
+  def editor?
+    role == 'editor'
+  end
+  
+  def author?
+    role == 'author'
+  end
+
+  def intern?
+    role == 'intern'
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140925034326) do
+ActiveRecord::Schema.define(version: 20140925045754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20140925034326) do
     t.datetime "last_logout_at"
     t.datetime "last_activity_at"
     t.string   "last_login_from_ip_address"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
